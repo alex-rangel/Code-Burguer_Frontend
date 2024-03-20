8705e3bb6aa3faba0920359e32d23fa364f250b4
alex-rangel <alexrangel90@gmail.com>
2024-03-20 05:31:35 -0300

    finalizei o carrocel de categorias

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-//import Carousel from 'react-elastic-carousel';
+import Carousel from 'react-elastic-carousel';
 
 import categorias from '../../assets/CATEGORIAS.png'
-import { Container, CategoryImg, ImgCarrossel } from "./style";
+import { Container, CategoryImg, ContainerItems, Image, Button } from "./style";
 
 import api from '../../services/api'
 
@@ -16,12 +16,34 @@ function CategoryCarousel() {
         }
         loadCategories()
     }, [])
+
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 400, itemsToShow: 2 },
+        { width: 600, itemsToShow: 3 },
+        { width: 900, itemsToShow: 4 },
+        { width: 1300, itemsToShow: 5 }
+    ]
+
     return (
         <Container>
             <CategoryImg src={categorias} alt="Imagem do titulo do carrossel de categotias" />
-            { categories.map(category => (
-                <ImgCarrossel key={category.id} src={category.url} alt="Imagem teste"/>
-            )) }
+
+            <Carousel 
+            itemsToShow={5}
+            style={{width: '90%'}}
+            breakPoints={breakPoints}>
+                {categories &&
+                    categories.map(category => (
+                        <ContainerItems key={category.id}>
+                            <Image src={category.url} />
+                            <Button>
+                                {category.nome}
+                            </Button>
+                        </ContainerItems>
+                    ))}
+            </Carousel>
+
         </Container>
     )
 }
