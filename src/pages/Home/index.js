292296e0911aa91caf9e925e85c7f3d27214bbdf
alex-rangel <alexrangel90@gmail.com>
2024-03-20 22:29:22 -0300

    finalizei a tela de home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,14 @@ import banerHome from '../../assets/banerHome.svg'
 import { Container, HomeImg } from "../Home/style";
 
 import CategoryCarousel from '../../components/CategoryCarousel'
+import OffersCarousel from '../../components/OffersCarousel'
 
 function Home() {
     return(
         <Container>
             <HomeImg src={banerHome} alt="baner da pagina home"/>
             <CategoryCarousel />
+            <OffersCarousel/>
         </Container>
     )
 }
